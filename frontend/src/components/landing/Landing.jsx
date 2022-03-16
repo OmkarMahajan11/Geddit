2e48293982a933865a32a9b91a54c297b1691efb
Omkar Mahajan <omkar.mahajan@sarasanalytics.com>
2022-03-16 15:13:06 +0530

    Revert "start over"
    This reverts commit 30bb1da473ff59909b6760f58f763a0d8915495b.

---
+++ frontend/src/components/landing/Landing.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import Navbar from "../navbar/Navbar";
+
+const Landing = () => {
+  return (
+    <>
+      <Navbar />
+    </>
+  );
+};
+
+export default Landing;
---
+++ frontend/src/components/navbar/logo/Logo.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+import "./Logo.css";
+
+const Logo = () => {
+  return (
+    <div className="logo hoverable">
+      <img src="./assets/images/reddit-logo.png" alt="" />
+      <span>reddit</span>
+    </div>
+  );
+};
+
+export default Logo;
